a81f70d864b5810be3d8baf394e8955c90d524c0
Adrian Miller <adrianjonmiller@gmail.com>
2016-08-03 14:06:05 -0700

    rolled back js

--- themes/starkers-child/js/behaviors/behaviors.js
+++ themes/starkers-child/js/behaviors/behaviors.js
@@ -52,43 +52,13 @@ jQuery(document).ready(function($) {
 
 
 	Js.Behaviors.flexslider = function(container){
-		container.flexslider({
+		$(container).flexslider({
 				controlNav: false,
 				directionNav: false
 		});
 	}
 
 	Js.Behaviors.maxImageWidth = function(container){
-		container.children().maxImageWidth();
-	}
-
-	Js.Behaviors.freeSample = function(container) {
-		console.log(container);
-		$(container).submit(function(e){
-			e.preventDefault();
-
-			var formData = {
-				'first_name'              : $('input[name="first_name"]').val(),
-				'last_name'               : $('input[name="last_name"]').val(),
-				'company_name'            : $('input[name="company_name"]').val(),
-				'email'            			  : $('input[name="email"]').val(),
-				'phone_number'            : $('input[name="phone_number"]').val(),
-				'address'            		  : $('input[name="address"]').val(),
-				'city'              			: $('input[name="city"]').val(),
-				'state'             		  : $('select[name="state"]').val(),
-				'zip_postal_code'					: $('input[name="zip_postal_code"]').val(),
-				'country'            		  : $('input[name="country"]').val(),
-				'product_type'            : $('select[name="product_type"]').val()
-			}
-
-			$.ajax({
-					type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-					url         : '/wp-content/themes/starkers-child/_order-free-sample.php', // the url where we want to POST
-					data        : formData // our data object
-			}).done(function(data) {
-			}).success(function(data){
-				$(container).html("Thank you for your submission.");
-			})
-		});
+		$(container).children().maxImageWidth();
 	}
 });
